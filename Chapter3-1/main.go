e817323063f843aafc868fc6e1c8303766c817df
Komkrit_sf <jokez.line@hotmail.com>
2018-02-07 10:19:52 +0700

    Add Chapter3-1.go

---
+++ Chapter3-1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("===== Zero value =====")
+	var number int
+	var str string
+	var boolean bool
+	fmt.Printf("number : %v\n",number)
+	fmt.Printf("string : %v\n",str)
+	fmt.Printf("boolean : %v\n",boolean)
+}
