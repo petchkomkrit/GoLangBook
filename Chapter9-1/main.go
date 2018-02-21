62d922ba3b8bea6af2fb4894ca3a773abca77ec0
Komkrit_sf <jokez.line@hotmail.com>
2018-02-21 10:30:34 +0700

    Modified Chapter9-1

--- Chapter9-1/main.go
+++ Chapter9-1/main.go
@@ -16,4 +16,17 @@ func main() {
 	c1 := new(Circle)
 	fmt.Printf("c1 type : %T\n", c1)
 	fmt.Println(c1.x, c1.y, c1.r)
+
+	c2 := Circle{x: 0, y: 0, r: 0}
+	fmt.Printf("c2 type : %T\n", c2)
+	fmt.Println(c2.x, c2.y, c2.r)
+
+	c3 := NewCircle(1, 2, 3)
+	fmt.Printf("c3 type : %T\n", c3)
+	fmt.Println(c3.x, c3.y, c3.r)
+
+}
+
+func NewCircle(x, y, r float64) *Circle {
+	return &Circle{x, y, r}
 }
