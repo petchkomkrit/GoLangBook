8500d0a934321ccff5498bcf8032012ce64df38c
Komkrit_sf <jokez.line@hotmail.com>
2018-03-09 10:57:35 +0700

    speedy espresso ver2

--- exercise.2/speedy-espresso.go
+++ exercise.2/speedy-espresso.go
@@ -27,8 +27,10 @@ func order(volumn int) (container []string) {
 	go barista(order, order1)
 	go waiter(order1, order2)
 
-	orderFinish := <-order2
-	container = append(container, orderFinish)
+	for orderFinish := range order2 {
+		//orderFinish := <-order2
+		container = append(container, orderFinish)
+	}
 
 	return
 }
@@ -52,12 +54,17 @@ func barista(orderIn <-chan string, orderOut chan<- string) {
 		coffee = fmt.Sprintf("%s %s", coffee, "Espresso")
 		orderOut <- coffee
 	}
+	close(orderOut)
 }
 
 func waiter(orderIn <-chan string, orderOut chan<- string) {
-	//waiter
-	coffee := <-orderIn
-	time.Sleep(5 * time.Millisecond)
-	coffee = fmt.Sprintf("%s %s", coffee, "ready :)")
-	orderOut <- coffee
+
+	for coffee := range orderIn {
+		//waiter
+		//coffee := <-orderIn
+		time.Sleep(5 * time.Millisecond)
+		coffee = fmt.Sprintf("%s %s", coffee, "ready :)")
+		orderOut <- coffee
+	}
+	close(orderOut)
 }
